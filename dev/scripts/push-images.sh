35dbb01df5948f3de6d810256fae968b252939de
Diego Cattelan <cattelan.diego@gmail.com>
2025-04-05 01:36:14 +0200

    feat: makefile

--- dev/scripts/push-images.sh
+++ dev/scripts/push-images.sh
@@ -3,6 +3,13 @@
 source .env
 
 COMPOSE_FILE="docker-compose.yml"
+ROOT_DIR="dev/docker"
 
-echo "🐳 Docker Compose: PUSH"
-docker compose -f "$COMPOSE_FILE" push
+# entra nella cartella root di docker build
+cd "$ROOT_DIR" || { echo "Directory non trovata: $ROOT_DIR"; exit 1; }
+
+for dir in */; do
+  dir_name=$(basename "$dir")
+  docker compose -f "$COMPOSE_FILE" push ${dir_name}
+done
+
