d936650f83231e2897e781e133f87de1d8482454
Diego Cattelan <cattelan.diego@gmail.com>
2025-04-05 10:37:25 +0200

    feat: makefile

--- dev/scripts/push-images.sh
+++ dev/scripts/push-images.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-source .env
-
 COMPOSE_FILE="./docker-compose.yml"
 ROOT_DIR="dev/docker"
 export DOCKER_CONFIG=/workdir/.docker
-cat $DOCKER_CONFIG/config.json
 
-env
-tree -apug -I .git -I .idea
-echo "docker info------------------------------------"
-docker info
-echo "docker context ls------------------------------"
-docker context ls
-echo "docker compose config--------------------------"
-docker compose config
+if [ -z "$DOCKERHUB_USERNAME" ]; then
+  echo "❌ Errore: DOCKERHUB_USERNAME non è valorizzata o è vuota!"
+  exit 1
+else
+  echo "✅ DOCKERHUB_USERNAME è valorizzata!"
+fi
+
+if [ -z "$DOCKERHUB_TOKEN" ]; then
+  echo "❌ Errore: DOCKERHUB_TOKEN non è valorizzata o è vuota!"
+  exit 1
+else
+  echo "✅ DOCKERHUB_TOKEN è valorizzata!"
+fi
+
+echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+
 
 docker compose -f "$COMPOSE_FILE" push
-
-# entra nella cartella root di docker build
-#cd "$ROOT_DIR" || { echo "Directory non trovata: $ROOT_DIR"; exit 1; }
-#
-#for dir in */; do
-#  dir_name=$(basename "$dir")
-#  docker compose -f "$COMPOSE_FILE" push ${dir_name} --include-deps
-#done
 
