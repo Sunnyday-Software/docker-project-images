a1f02d463312088773f2a7e09e4d150584d21cf3
Diego Cattelan <cattelan.diego@gmail.com>
2025-04-05 09:41:14 +0200

    feat: makefile

--- dev/scripts/docker-login.sh
+++ dev/scripts/docker-login.sh
@@ -5,5 +5,19 @@ source .env
 COMPOSE_FILE="./docker-compose.yml"
 ROOT_DIR="dev/docker"
 
+if [ -z "$DOCKERHUB_USERNAME" ]; then
+  echo "❌ Errore: DOCKERHUB_USERNAME non è valorizzata o è vuota!"
+  exit 1
+else
+  echo "✅ DOCKERHUB_USERNAME è valorizzata!"
+fi
+
+if [ -z "$DOCKERHUB_TOKEN" ]; then
+  echo "❌ Errore: DOCKERHUB_TOKEN non è valorizzata o è vuota!"
+  exit 1
+else
+  echo "✅ DOCKERHUB_TOKEN è valorizzata!"
+fi
+
 echo "Docker login with user $DOCKERHUB_TOKEN"
 echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
