c12ba251a0d4c2b2d09bf86730ff865ee6d1d939
Diego Cattelan <cattelan.diego@gmail.com>
2025-04-05 09:34:29 +0200

    feat: makefile

--- dev/scripts/docker-login.sh
+++ dev/scripts/docker-login.sh
@@ -5,5 +5,5 @@ source .env
 COMPOSE_FILE="./docker-compose.yml"
 ROOT_DIR="dev/docker"
 
-echo "Docker login"
+echo "Docker login with user $DOCKERHUB_TOKEN"
 echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
