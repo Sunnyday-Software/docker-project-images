4c5e35c46aa643ddbf4e9333c3163814f172590e
Diego Cattelan <cattelan.diego@gmail.com>
2025-09-20 15:16:46 +0200

    feat: cdk8s

--- build-for-dev.sh
+++ build-for-dev.sh
@@ -9,17 +9,23 @@ build_image() {
   local image_name="$2"
   local base_image_name="$3"
 
-  docker build --no-cache --pull \
+  cmd=(docker build --no-cache --pull \
     --platform $PLATFORM \
     --build-arg PLATFORM_TAG="$PLATFORM_TAG" \
     --build-arg IMAGE_FULL_NAME="sunnydaysoftware/${image_name}:localdev"  \
     --build-arg BASE_IMAGE="${base_image_name}" \
     -t $image_name:localdev \
-    "$image_base_path/$image_name"
+    "$image_base_path/$image_name")
+
+  printf 'Eseguo:'
+  printf ' %q' "${cmd[@]}"
+  printf '\n'
+
+  "${cmd[@]}"
 
 }
 
 #build_image "dev/docker" "bash"
 #build_image "dev/docker" "make"
-build_image "dev/docker" "cdk8s"
+build_image "dev/docker" "cdk8s" "sunnydaysoftware/bash:v-15"
 #build_image "dev/docker" "node-semantic-release" "bash:localdev"
